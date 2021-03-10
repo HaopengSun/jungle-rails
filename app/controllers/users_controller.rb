f223a347ba3912054f92b51f68c904a154e3eeb0
HaopengSun <sunhaopeng@gmail.com>
2021-03-10 23:33:57 +0000

    fix user create controller bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,9 @@ class UsersController < ApplicationController
   end
   
   def create
-    user_params[:email] = user_params[:email].downcase.strip
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
